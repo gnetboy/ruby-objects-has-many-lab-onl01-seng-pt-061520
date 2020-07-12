75ab0a8b39a17286f29146f99dcfe01d8a32e18e
Gustavo Hernandez <gnetboy.y@gmail.com>
2020-07-12 04:12:44 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,12 +9,10 @@ class Artist
     
     def add_song(song)
         song.artist = self
-        
-        
     end
-
+    
     def songs
-        Song.all.select {|song|song.artist == self}
+      Song.all.select {|song| song.artist == self}
     end
 
     def add_song_by_name(name)
